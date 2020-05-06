118c40cf4d242900d55ac0f4af5541f99939a64f
RonnyFalconeri <ronny@falconeri.de>
2020-05-06 15:53:42 +0200

    layout design

--- web/src/layouts/DefaultContainerLayout/DefaultContainerLayout.js
+++ web/src/layouts/DefaultContainerLayout/DefaultContainerLayout.js
@@ -1,16 +1,49 @@
+import '../../../public/css/styling.css';
+
 const DefaultContainerLayout = ({ children }) => {
   return (
-    <div>
+    <div style={styles.container}>
 
-      <div style={{ width: '100%', height: 100, backgroundColor: 'red', textAlign: 'center' }}>
-        <h1>CookIT! Web Edition</h1>
+      <div style={styles.header}>
+        <h1>CookIT! Web Edition<img src="../../../public/images/icon.png" style={styles.headerIcon} /></h1>
       </div>
 
       <main>{children}</main>
 
-      <p>&copy; Falconeri, Schindler</p>
+      <div style={styles.footer}>
+        <p style={styles.footerText}>&copy; Falconeri, Schindler</p>
+      </div>
+
     </div>
   )
 }
+
+const styles = {
+  container: {
+    backgroundColor: '#f3f3f3',
+    height: '100vh',
+    paddingTop: 120
+  },
+  header: {
+    width: '100%',
+    height: 110,
+    position: 'absolute',
+    backgroundColor: 'white',
+    top: 0,
+    boxShadow: '0 0 10px 0 #000'
+  },
+  headerIcon: {
+    height: 80
+  },
+  footer: {
+    height: 30,
+    position: 'absolute',
+    bottom: 0,
+    width: '100%'
+  },
+  footerText: {
+    textAlign: 'center'
+  }
+};
 
 export default DefaultContainerLayout
