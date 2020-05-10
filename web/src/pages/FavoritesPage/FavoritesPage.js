f32257dbd8233ee39504cfc63d76759a3cefdbf8
RonnyFalconeri <ronny@falconeri.de>
2020-05-10 18:11:59 +0200

    ListItems exported to components

--- web/src/pages/FavoritesPage/FavoritesPage.js
+++ web/src/pages/FavoritesPage/FavoritesPage.js
@@ -1,4 +1,5 @@
 import DefaultContainerLayout from 'src/layouts/DefaultContainerLayout'
+import FavListItem from 'src/components/FavListItem'
 
 const FavoritePage = () => {
   return (
@@ -6,20 +7,11 @@ const FavoritePage = () => {
 
       <div style={styles.list}>
 
-        <div
-          style={styles.listItem}
-          onClick={() => navigate(routes.recipes())}
-        >
-          <img src="../../../public/images/recipeDefaultImage.png" style={styles.image} />
-          <h2 style={styles.title}>Spaghetti Bolognese</h2>
-
-          <div style={styles.durationColumn}>
-            <p style={styles.duration}>14 Min</p>
-            <p style={styles.nationality}> ITA</p>
-          </div>
-
-        </div>
-
+        <FavListItem
+          title="Spaghetti Bolognese"
+          duration="14"
+          nationality="ITA"
+        />
 
       </div>
 
@@ -34,40 +26,5 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'column'
-  },
-  listItem: {
-    margin: 20,
-    height: 100,
-    width: '80%',
-    borderRadius: 20,
-    backgroundColor: '#8e7cc3',
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    padding: 10,
-    cursor: 'pointer',
-    boxShadow: '0 4px 4px 0 grey'
-  },
-  title: {
-    color: 'white'
-  },
-  image: {
-    height: 70,
-    width: 120,
-    border: '1px solid black',
-    borderRadius: 3
-  },
-  durationColumn: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  duration: {
-    color: 'white',
-    margin: 5
-  },
-  nationality: {
-    color: 'white',
-    margin: 5
   }
 };
--- web/src/pages/RecipesPage/RecipesPage.js
+++ web/src/pages/RecipesPage/RecipesPage.js
@@ -1,5 +1,5 @@
-import { routes, navigate } from '@redwoodjs/router'
 import DefaultContainerLayout from 'src/layouts/DefaultContainerLayout'
+import RecipesListItem from 'src/components/RecipesListItem'
 
 const RecipesPage = () => {
   return (
@@ -7,19 +7,11 @@ const RecipesPage = () => {
 
       <div style={styles.list}>
 
-        <div
-          style={styles.listItem}
-          onClick={() => navigate(routes.recipe())}
-        >
-          <img src="../../../public/images/recipeDefaultImage.png" style={styles.image} />
-          <h2 style={styles.title}>Spaghetti Bolognese</h2>
-
-          <div style={styles.durationColumn}>
-            <p style={styles.duration}>14 Min</p>
-            <p style={styles.nationality}> ITA</p>
-          </div>
-
-        </div>
+        <RecipesListItem
+          title="Spaghetti Bolognese"
+          duration="14"
+          nationality="ITA"
+        />
 
 
       </div>
@@ -35,40 +27,5 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'column'
-  },
-  listItem: {
-    margin: 20,
-    height: 100,
-    width: '80%',
-    borderRadius: 20,
-    backgroundColor: '#6d9eeb',
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    padding: 10,
-    cursor: 'pointer',
-    boxShadow: '0 4px 4px 0 grey'
-  },
-  title: {
-    color: 'white'
-  },
-  image: {
-    height: 70,
-    width: 120,
-    border: '1px solid black',
-    borderRadius: 3
-  },
-  durationColumn: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  duration: {
-    color: 'white',
-    margin: 5
-  },
-  nationality: {
-    color: 'white',
-    margin: 5
   }
 };
